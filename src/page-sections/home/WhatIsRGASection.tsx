2c99fd37eafdac7c3c4f17a23af05c77329210e1
Samir BENFARES <sbenfares@gmail.com>
2025-08-27 12:57:46 +0200

    feat: what is rga end

--- src/page-sections/home/WhatIsRGASection.tsx
+++ src/page-sections/home/WhatIsRGASection.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
 import wording from "@/wording";
+import { Feature } from "@/components";
 
 export default function WhatIsRgaSection() {
   return (
-    <section className="fr-container-fluid fr-py-10w bg-[var(--background-default-grey-hover)]">
+    <section className="fr-container-fluid fr-py-10w">
       <div className="fr-container">
         {/* Zone Hero Qu'est-ce que le RGA */}
         <div className="fr-grid-row items-center gap-6 md:gap-0">
@@ -36,8 +37,120 @@ export default function WhatIsRgaSection() {
         </div>
 
         {/* Zone Icônes & Infos */}
+        <div className="fr-grid-row fr-grid-row--gutters fr-mt-8w">
+          <div className="fr-col-12 fr-col-sm-6 fr-col-md-3">
+            <Feature
+              icon="fr-icon-heavy-showers-fill"
+              title="Période humide"
+              description="Les sols argileux gonflent en absorbant l'eau, exerçant une pression sur les fondations."
+            />
+          </div>
+          <div className="fr-col-12 fr-col-sm-6 fr-col-md-3">
+            <Feature
+              icon="fr-icon-sun-fill"
+              title="Période sèche"
+              description="Les sols se rétractent en perdant leur humidité, créant des mouvements de terrain."
+              tileColor="bg-orange-50"
+              iconColor="text-orange-500"
+            />
+          </div>
+          <div className="fr-col-12 fr-col-sm-6 fr-col-md-3">
+            <Feature
+              icon="fr-icon-refresh-line"
+              title="Cycles répétitifs"
+              description="Ces alternances humides/sèches se répètent dans le temps fragilisant sols et fondations."
+              tileColor="bg-purple-50"
+              iconColor="text-purple-500"
+            />
+          </div>
+          <div className="fr-col-12 fr-col-sm-6 fr-col-md-3">
+            <Feature
+              icon="fr-icon-home-4-fill"
+              title="Conséquences"
+              description="Fissures des murs, déformation des ouvertures, désalignement des portes et fenêtres."
+              tileColor="bg-red-50"
+              iconColor="text-red-600"
+            />
+          </div>
+        </div>
 
         {/* Zone Signes à surveiller */}
+        <div className="container fr-mt-8w">
+          <h1>Les signes à surveiller</h1>
+          <div className="fr-grid-row fr-grid-row--gutters">
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Fissures en escalier sur les murs extérieurs
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Fissures horizontales le long des plinthes
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Portes et fenêtre qui ferment mal
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Décollements entre la cloison et le plafond
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Affaissement ou soulèvement de dallages
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="fr-col-12 fr-col-md-4 fr-col-lg-3">
+              <div className="fr-tile" id="tile-10">
+                <div className="fr-tile__body">
+                  <div className="fr-tile__content">
+                    <h6 className="fr-tile__title" style={{ fontSize: "16px" }}>
+                      Fissures dans les cloisons intérieures
+                    </h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
