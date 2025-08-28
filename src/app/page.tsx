ced0f655c6a7204e19ec0f845e8d5cabc45cfeff
Samir BENFARES <sbenfares@gmail.com>
2025-08-28 13:15:59 +0200

    feat: en savoir plus section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Notice } from "@/components";
 import { contentHomePage } from "@/content";
 import { EtatAgitSection, CommentCaMarcheSection } from "@/page-sections";
 import LogementConcerneRgaSection from "@/page-sections/home/LogementConcerneRgaSection";
+import PourEnSavoirPlusSection from "@/page-sections/home/PourEnSavoirPlusSection";
 import QuelsSontTravauxEligiblesSection from "@/page-sections/home/QuelsSontTravauxEligiblesSection";
 import QuestCeQueLeRgaSection from "@/page-sections/home/QuestCeQueLeRgaSection";
 import QuiPeutBeneficierAidesSection from "@/page-sections/home/QuiPeutBeneficierAidesSection";
@@ -22,6 +23,7 @@ export default function Home() {
         <CommentCaMarcheSection />
         <QuiPeutBeneficierAidesSection />
         <QuelsSontTravauxEligiblesSection />
+        <PourEnSavoirPlusSection />
       </div>
     </>
   );
---
+++ src/page-sections/home/PourEnSavoirPlusSection.tsx
@@ -0,0 +1,51 @@
+import { contentHomePage } from "@/content";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PourEnSavoirPlusSection() {
+  return (
+    <section className="fr-container-fluid fr-py-10w bg-gray-100">
+      <div className="fr-container">
+        <h1>{contentHomePage.pour_en_savoir_plus_section.title}</h1>
+        <div className="fr-grid-row fr-grid-row--gutters">
+          {contentHomePage.pour_en_savoir_plus_section.cards.map(
+            (card, index) => (
+              <div
+                key={`${index}-card`}
+                className="fr-col-12 fr-col-md-6 fr-col-lg-3"
+              >
+                <div className="fr-card fr-enlarge-link">
+                  <div className="fr-card__body">
+                    <div className="fr-card__content">
+                      <h3 className="fr-card__title">
+                        <Link
+                          href={card.linkUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {card.title}
+                        </Link>
+                      </h3>
+                      <p className="fr-card__desc">{card.description}</p>
+                    </div>
+                  </div>
+                  <div className="fr-card__header">
+                    <div className="fr-card__img">
+                      <Image
+                        className="fr-responsive-img"
+                        src={card.imageUrl}
+                        alt={card.imageAlt}
+                        width={350}
+                        height={125}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
