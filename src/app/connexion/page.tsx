51aa0ec085679c49e281d86d4017799000837b11
Samir BENFARES <sbenfares@gmail.com>
2025-10-10 10:06:43 +0200

    reset connection

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -1,13 +1,11 @@
 import Loading from "@/components/Loading/Loading";
-// import ConnexionFranceConnectClient from "@/page-sections/connexion/ConnexionFranceConnectClient";
+import ConnexionFranceConnectClient from "@/page-sections/connexion/ConnexionFranceConnectClient";
 import { Suspense } from "react";
-import EarlyAccessForm from "../simulateur/components/EarlyAccessForm";
 
 export default function ConnexionPage() {
   return (
     <Suspense fallback={<Loading />}>
-      {/* <ConnexionFranceConnectClient /> */}
-      <EarlyAccessForm />
+      <ConnexionFranceConnectClient />
     </Suspense>
   );
 }
