a9c46cd54a4ec5796b12fb40e0481dc6c7b0a75d
Samir BENFARES <sbenfares@gmail.com>
2025-10-14 14:52:48 +0200

    feat: remove from connexion

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -1,13 +1,11 @@
 import Loading from "@/app/loading";
 import ConnexionFranceConnectClient from "@/page-sections/connexion/ConnexionFranceConnectClient";
 import { Suspense } from "react";
-// import EarlyAccessForm from "../simulateur/components/EarlyAccessForm";
 
 export default function ConnexionPage() {
   return (
     <Suspense fallback={<Loading />}>
       <ConnexionFranceConnectClient />
-      {/* <EarlyAccessForm /> */}
     </Suspense>
   );
 }
