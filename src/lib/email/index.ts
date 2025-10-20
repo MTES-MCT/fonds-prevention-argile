5b897eaf138690a30e05a72d0fcf3895c4074f86
Samir BENFARES <sbenfares@gmail.com>
2025-10-20 08:43:37 +0200

    refactor(phase1): Architecture DDD

--- src/lib/email/index.ts
+++
@@ -1,5 +0,0 @@
-export { renderEmailTemplate } from "./render-template";
-export { EmailLayout, EmailComponents } from "./templates/EmailLayout";
-export { ValidationAmoTemplate } from "./templates/validation-amo.template";
-export { emailConfig, isEmailEnabled } from "./config";
-export { emailService } from "./email.service";
