8072583fbdd95a00b02028369464ab3447f72978
Samir BENFARES <sbenfares@gmail.com>
2025-12-02 11:10:10 +0100

    feat: add sitemap & robots

---
+++ src/app/robots.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://fonds-prevention-argile.beta.gouv.fr";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/administration/",
+          "/espace-amo/",
+          "/mon-compte/",
+          "/connexion/agent/",
+          "/deconnexion/",
+          "/oidc-callback/",
+          "/embed-simulateur/",
+        ],
+      },
+    ],
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  };
+}
