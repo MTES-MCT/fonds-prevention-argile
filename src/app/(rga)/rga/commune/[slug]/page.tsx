db3b71dee03c3fc4413ffbdc5a208e1f29bc019f
Samir BENFARES <sbenfares@gmail.com>
2025-12-09 15:08:29 +0100

    feat: add SEO_REVALIDATE_TIME

--- src/app/(rga)/rga/commune/[slug]/page.tsx
+++ src/app/(rga)/rga/commune/[slug]/page.tsx
@@ -28,9 +28,13 @@ import {
 import templateContent from "../content/template.json";
 import SavoirSiConcerneSection from "@/app/(main)/(home)/components/SavoirSiConcerneSection";
 import { richTextParser } from "@/shared/utils";
+import { SEO_REVALIDATE_TIME } from "@/features/seo/domain";
 
 // Nombre de communes à afficher
 const NB_COMMUNES_A_AFFICHER = 8;
+
+// Durée de revalidation ISR
+export const revalidate = SEO_REVALIDATE_TIME;
 
 interface PageProps {
   params: Promise<{
