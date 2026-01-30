ae27b1ffa7faf240520ee4a130f5216b5ca3534c
Samir BENFARES <sbenfares@gmail.com>
2026-01-30 15:55:09 +0100

    fix: link to a

--- src/features/simulateur/components/shared/SimulateurLayout.tsx
+++ src/features/simulateur/components/shared/SimulateurLayout.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from "react";
 import { ProgressBar } from "./ProgressBar";
-import Link from "next/link";
 
 interface SimulateurLayoutProps {
   children: ReactNode;
@@ -35,14 +34,14 @@ export function SimulateurLayout({
         <div className="fr-grid-row fr-grid-row--center">
           <div className="fr-col-12 fr-col-md-8 fr-col-lg-8 md:bg-[var(--background-alt-grey)] p-0 md:p-10">
             <div className="flex justify-end fr-mb-2w px-4 pt-4 md:px-0 md:pt-0">
-              <Link
+              <a
                 id="link-help"
                 href="mailto:contact@fonds-prevention-argile.fr?subject=Besoin%20d'aide%20pour%20le%20simulateur%20d'éligibilité%20au%20Fonds%20Prévention%20Argile"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="fr-link fr-icon-question-fill fr-link--icon-left">
                 Besoin d'aide ?
-              </Link>
+              </a>
             </div>
             <div className="px-4 md:px-8 pb-4 md:pb-0 fr-mt-4w md:fr-mt-6w">
               <h5 className="fr-mb-4w">Simulateur d'éligibilité au Fonds Prévention Argile</h5>
