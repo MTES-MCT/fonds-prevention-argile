3fc5d7d21a23392d3e1ebf1b980cf041e72bd6f7
Martin Letellier <dinum-327867@MartinLnsOublis.lan>
2026-05-29 15:15:01 +0200

    utilisation du nom de famille et pas uniquement nom de jeune fille dans les cas de changements de nom au mariage

--- src/shared/database/repositories/user.repository.ts
+++ src/shared/database/repositories/user.repository.ts
@@ -167,7 +167,7 @@ export class UserRepository extends BaseRepository<User> {
         fcId: userInfo.sub,
         // L'email FC est autoritaire si fourni ; sinon on garde celui saisi par l'AV.
         email: userInfo.email || sql`${users.email}`,
-        nom: userInfo.family_name || sql`${users.nom}`,
+        nom: userInfo.usual_name || userInfo.family_name || sql`${users.nom}`,
         prenom: userInfo.given_name || sql`${users.prenom}`,
         claimedAt: new Date(),
         claimToken: null,
@@ -219,7 +219,7 @@ export class UserRepository extends BaseRepository<User> {
       const updates: Partial<NewUser> = {
         lastLogin: new Date(),
         email: userInfo.email || existingUser.email,
-        nom: userInfo.family_name || existingUser.nom,
+        nom: userInfo.usual_name || userInfo.family_name || existingUser.nom,
         prenom: userInfo.given_name || existingUser.prenom,
       };
 
@@ -249,7 +249,7 @@ export class UserRepository extends BaseRepository<User> {
     return await this.create({
       fcId,
       email: userInfo.email,
-      nom: userInfo.family_name,
+      nom: userInfo.usual_name || userInfo.family_name,
       prenom: userInfo.given_name,
       lastLogin: new Date(),
       partnerSource: options?.partnerSource ?? null,
